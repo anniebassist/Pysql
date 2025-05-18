05625482e2cada1bbb2c28e28cd80a9aad8484c6
John Doe <johndoe@email.com>
2025-05-18 19:40:17 +0200

    added another change

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def getConfirmation():
 
 @app.get("/test")
 def getConfirmation():
- return "check test"
+ return "check new test please"
 
 #return albums for a particular band
 @app.get("/getBandbyID/{id}")
